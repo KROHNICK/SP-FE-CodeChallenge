f44ad6d6741352d3a5ae6b38821a485e26d21971
Kanek Perez <business.kanekperez@gmail.com>
2020-04-06 11:37:48 -0700

    Using redux for shopping cart state management, created db for products

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import Header from "./components/header/header";
 import jacket from "./1.jpg";
 import shorts from "./2.jpg";
 import shoes from "./3.jpg";
+import axios from "axios";
+import { Route, NavLink } from "react-router-dom";
 
 class App extends Component {
   constructor() {
@@ -12,6 +14,28 @@ class App extends Component {
         cart: []
     };
   }
+
+  componentDidMount() {
+    console.log(this.state);
+  }
+
+  addToCart = e => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:3000/cart", this.state)
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+      this.setState({
+        name: ""
+      });
+  }
+
+
+
 
   render() {
     return (
@@ -22,7 +46,7 @@ class App extends Component {
                     <img src={jacket} className="prod" alt="" />
                     <div className="sub">
                         <p>$PRICE</p>
-                        <button>Add to cart</button>
+                        <button onClick={this.addToCart} >Add to cart</button>
                     </div>
                 </div>
 
